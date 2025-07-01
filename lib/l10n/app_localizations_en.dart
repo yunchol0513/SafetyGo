4a2c27d3eac5ce86a38a4056fb334fc5c5c0cdb8
taisei <e1923069@oit.ac.jp>
2025-07-01 15:10:02 +0900

    main

---
+++ lib/l10n/app_localizations_en.dart
@@ -0,0 +1,64 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for English (`en`).
+class AppLocalizationsEn extends AppLocalizations {
+  AppLocalizationsEn([String locale = 'en']) : super(locale);
+
+  @override
+  String get languageTitle => 'English';
+
+  @override
+  String get selectEnglish => 'English';
+
+  @override
+  String get selectJapanese => 'Japanese';
+
+  @override
+  String get next => 'Next';
+
+  @override
+  String get loginTitle => 'Login';
+
+  @override
+  String get back => 'Back';
+
+  @override
+  String get genreTitle => 'Select Genre';
+
+  @override
+  String get modifyLanguage => 'Change Language';
+
+  @override
+  String get disaster => 'Disaster';
+
+  @override
+  String get detaileTitle => 'Select Detaile';
+
+  @override
+  String get quake => 'Earthquake';
+
+  @override
+  String get diffcultyTitle => 'Select Diffculty';
+
+  @override
+  String get easy => 'Easy';
+
+  @override
+  String get normal => 'Normal';
+
+  @override
+  String get hard => 'Hard';
+
+  @override
+  String get quakeEasy => 'Earthquake～Easy～';
+
+  @override
+  String get study => 'Study';
+
+  @override
+  String get question => 'Question';
+}
